95cb4f3e8f0fbdb7052fcda910b1b764b1375a20
eoleahy <eoin.97@live.ie>
2020-01-13 17:55:51 +0000

    added bare privacy policy

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
+import datetime
 app = Flask(__name__)
 
+@app.route('/', methods = ['POST','GET'])
+def create_policy():
 
-@app.route('/')
-def hello_world():
-    return render_template('home.html', username = "Eoin")
+    company_name = ""
 
+    if request.method == 'POST':
+        company_name = request.form["company_name"]
+
+
+    return render_template('create_policy.html')    
+
+@app.route('/policy', methods = ['POST', 'GET'])
+def policy():
+
+    company_name = "Eoin"
+
+    date = datetime.date.today()
+    date = date.strftime("%B %d. %Y")
+
+    topics = ["What data do we collect?",
+                "How will we collect your data?",
+                "How will we use your date?",
+                "How do we store your data?",
+                "Marketing",
+                "What are your data protection rights?",
+                "Add more topics...",
+                "Changes to our privacy policy",
+                "How to contact us",
+                "How to contact the appropriate authorities?"]
+
+    types_of_data =["Name","Email addresss","Phone number"]            
+
+    return render_template('policy.html',
+                            company=company_name,
+                            date=date,
+                            topic_list = topics,
+                            types_of_data = types_of_data
+                            )
 
 
 
 if __name__ == '__main__':
-    app.run()    
+    app.run()
