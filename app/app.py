26b557b8e047222147b48732eed73c3da5d1b1a2
eoleahy <eoin.97@live.ie>
2020-03-02 19:56:21 +0000

    added hover boxes

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
-#from personalDataCategory import PersonalDataCategory
+from bs4 import BeautifulSoup
 import datetime
+import requests
 import rdflib
 import json
+import re
 import os
 import sys
 
@@ -15,13 +17,14 @@ if(sys.platform == "win32"):
 else:
     json_path = os.path.join(BASE_PATH, "static/inputExample.json")
 
-views = ["dpv","dataSubject","dataController","thirdParty"]
+views = ["dpv", "dataSubject", "dataController", "thirdParty"]
+dpv_url = "http://w3.org/ns/dpv#"
+
 
 @app.route('/', methods=['GET'])
 def policy():
 
     data = {}
-    
 
     with open(json_path) as f:
         data = json.load(f)
@@ -32,10 +35,16 @@ def policy():
     purpose_set = set()
     collect_set = set()
 
+    data_classes = []
+
     for cat in data["personalDataCategory"]:
         purpose_set.update(cat["purpose"])
         collect_set.update(cat["collection"])
+        data_classes.append(cat["class"])
 
+    #print(data_classes)
+    dpvDescriptions = descriptions(data_classes)
+    #print(dpvDescriptions)
     data["date"] = date
 
     topics = [{"heading": "What data do we collect?", "page": "data.html"},
@@ -44,17 +53,35 @@ def policy():
               {"heading": "How do we store your data?", "page": "store.html"},
               {"heading": "Who do we share your data with?", "page": "share.html"},
               {"heading": "Cookies", "page": "cookies.html"},
-              {"heading": "What are your data protection rights?", "page": "rights.html"},
+              {"heading": "What are your data protection rights?",
+                  "page": "rights.html"},
               {"heading": "Changes to our privacy policy", "page": "changes.html"},
-              {"heading": "Data protection officer", "page": "dataprotection.html"},
+              {"heading": "Data protection officer",
+                  "page": "dataprotection.html"},
               {"heading": "How to contact us?", "page": "contact.html"}]
 
     return render_template('policy.html',
                            #dpv = g,
                            topics=topics,
                            data=data,
+                           dpv=dpvDescriptions,
                            purpose_set=purpose_set,
                            collect_set=collect_set)
+
+
+def descriptions(data):
+
+    page = requests.get(dpv_url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    descriptions = {x:get_description(soup,x.replace(" ", "-").lower()) for x in data}
+
+    return descriptions
+
+
+def get_description(soup, sectionId):
+
+    div = soup.find("section", {"id": sectionId})
+    return div
 
 
 if __name__ == '__main__':
